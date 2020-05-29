f66c1fa3fbf653f35f79e4a4117ee39237fa1961
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-29 22:06:59 +0300

    adds include path into the python build script to have acces to Python.h header file

--- projects/showdate/setup.py
+++ projects/showdate/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-module_0 = Extension('showdate', include_dirs=[], libraries=[
+module_0 = Extension('showdate', include_dirs=['usr/bin/include'], libraries=[
                      'pthread'], sources=['date.cc'],extra_compile_args=['-std=c++14'])
 
 setup(name='showdate', version='1.0', description='Returns a string from input and the corrent time and date',
