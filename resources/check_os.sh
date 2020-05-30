29fa025fda44ded492c297a789797b8eab435d8d
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-30 20:51:48 +0300

    minor change into build scripts. avoids double [[]] in shell script due to complains on Ubuntu and Debian

--- resources/check_os.sh
+++ resources/check_os.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-uname > os
-os_val="$(cat os)"
-darwin="Darwin"
-if [[ "$os_val" == "$darwin" ]]; then
+uname >os
+current_os="$(cat os)" # the platform on which the project is built
+darwin="Darwin"        # check if Apple's OSX
+if [ "$current_os" == "$darwin" ]; then
 	./build_darwin.sh
 else
 	./build_unix.sh
