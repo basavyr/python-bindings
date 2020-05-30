bdb92f9132ee4a005580ad30d4a1c754b5721be3
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-30 13:18:18 +0300

    adds py3 build and test on centos

--- projects/showdate/build_unix.sh
+++ projects/showdate/build_unix.sh
@@ -5,25 +5,23 @@ echo '*********************************'
 rm *.out
 rm -rf build/*
 /usr/bin/python2 setup.py build
-cd build/lib*
+cd build/lib.linux-x86_64-2.7 # the standard build folder for Linux on 2.7
 cp ../../test.py .
 /usr/bin/python2 test.py
-ls -la
-ld showdate.so
+# ld showdate.so  # this doesn't work on the bare centos image
 # # /usr/bin/python2 build/lib.macosx-10.15-x86_64-2.7/test.py
-# echo '*********************************'
-# echo 'Building the C++ extenstion for Python@3'
-# echo '*********************************'
+echo '*********************************'
+echo 'Building the C++ extenstion for Python@3'
+echo '*********************************'
 # # don't clear the build directory after the first build part
-# cd ..
-# cd ..  # go back in the main sub-project after buid on v2
-# /usr/bin/python3 setup.py build
-# echo '*********************************'
-# echo 'Testing the C++ extenstion for Python@3'
-# echo '*********************************'
-# cd build/lib.macosx-10.14*
-# cp ../../test.py .
-# /usr/bin/python3 test.py
+cd ../../ # go back in the main sub-project after buid on v2
+/usr/bin/python3 setup.py build
+echo '*********************************'
+echo 'Testing the C++ extenstion for Python@3'
+echo '*********************************'
+cd build/lib.linux-x86_64-3.7
+cp ../../test.py .
+/usr/bin/python3 test.py
 # # cp test.py build/lib.macosx-10.14-x86_64-3.7/test.py
 # # cd build/lib.macosx-10.14-x86_64-3.7/
 # # /usr/bin/python3 build/lib.macosx-10.14-x86_64-3.7/test.py
