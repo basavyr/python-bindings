1b9ff7d30de72cb5c64245c35df5ab89200cd73f
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-30 22:03:16 +0300

    adds the stdc++ lib in the python setup script for fixing demangled init name

--- projects/showdate/setup.py
+++ projects/showdate/setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup, Extension
 
+# needs the 'stdc++' library in order to compile with python3 on Debian and Ubuntu
 module_0 = Extension('showdate', include_dirs=['usr/bin/include'], libraries=[
-                     'pthread'], sources=['date.cc'],extra_compile_args=['-std=c++14'],language=['c++'])
+                     'pthread', 'stdc++'], sources=['date.cc'], extra_compile_args=['-std=c++14'], language=['c++'])
 
 setup(name='showdate', version='1.0', description='Returns a string from input and the corrent time and date',
       url='http://www.elk.nipne.ro', ext_modules=[module_0])
