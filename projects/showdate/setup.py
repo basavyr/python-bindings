79a8719c3382df48d554a0ab95e80071a235384d
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-30 22:24:45 +0300

    adds main reference into build script

--- projects/showdate/setup.py
+++ projects/showdate/setup.py
@@ -6,3 +6,6 @@ module_0 = Extension('showdate', include_dirs=['usr/bin/include'], libraries=[
 
 setup(name='showdate', version='1.0', description='Returns a string from input and the corrent time and date',
       url='http://www.elk.nipne.ro', ext_modules=[module_0])
+
+if __name__ == "__main__":
+    main()
