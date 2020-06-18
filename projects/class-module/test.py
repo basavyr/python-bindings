d3b0cd4653193606287d4c48bcb61c0676f9dca2
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-06-18 17:58:06 +0300

    adds method for initializing the class object. centos builds artifact with the python build script

--- projects/class-module/test.py
+++ projects/class-module/test.py
@@ -1,12 +1,31 @@
 import mclass
 
-mylist = [10, 20, 30]
-pynorm = 2
-pywhich = 3
-pywith = 9
-mystring = 'MCLass'
-mybstring = mystring.encode(encoding='utf-8')
-y = mclass.MClass(mylist, pynorm)
-# print(y.py_list)
-# print(y.py_norm)
-print(y.normalize())
+pyList = [10, 20, 30]
+pyNorm = 2
+pyWhich = 3
+pyWith = 9
+myString = 'MCLass'
+myByteString = myString.encode(encoding='utf-8')
+
+myVars = [pyList, pyNorm, pyWhich, pyWith, myByteString]
+
+
+def init_class(cls, vars, N_args):
+    if(N_args == 0):
+        retval = cls.MClass()
+    if(N_args == 1):
+        retval = cls.MClass(vars[0])
+    if(N_args == 2):
+        retval = cls.MClass(vars[0], vars[1])
+    if(N_args == 3):
+        retval = cls.MClass(vars[0], vars[1], vars[2])
+    if(N_args == 4):
+        retval = cls.MClass(vars[0], vars[1], vars[2], vars[3])
+    if(N_args == 5):
+        retval = cls.MClass(vars[0], vars[1], vars[2], vars[3], vars[4])
+    return retval
+
+
+for n_arg in range(6):
+    w = init_class(mclass, myVars, n_arg)
+    print(w.name())
